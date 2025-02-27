ff8d4333d2730e9e96c9b77243d52eca202ede9d
narayan <murgod@google.com>
2025-02-27 14:18:12 +0000

    blue

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,10 @@
-# Use official Python base image
-FROM python:3.9-slim
+FROM python:3.9
 
-# Set working directory
 WORKDIR /app
 
-# Copy requirements first to leverage Docker cache
-COPY requirements.txt .
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
 
-# Install dependencies
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Copy application code
 COPY . .
 
-# Expose port
-EXPOSE 5000
-
-# Command to run the application
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def blue_deployment():
-    return '''
-    <html>
-      <body style="background-color: blue; color: white;">
-        <h1>Blue Deployment</h1>
-        <p>Version 1.0</p>
-      </body>
-    </html>
-    '''
+@app.route("/")
+def home():
+    return "<body style='background-color:blue;'><h1 style='color:white;'>Blue Web Portal</h1></body>"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
